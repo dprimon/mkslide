444abbdccc5c714606e4f5620245ccd608f68861
Daniele Primon <ledebass@gmail.com>
2024-12-11 22:16:26 +0100

    Improve comment on desired ffmpeg command example

--- mk-slide.sh
+++ mk-slide.sh
@@ -153,5 +153,6 @@ else
   log "Something went wrong (error $ERR)."
 fi
 
-#template
+#example of desired ffmpeg command.
+#scale filters are not needed if preprocessing source images (e.g. with ImageMagick)
 #ffmpeg -loop 1 -t 3 -framerate 60 -i image1.jpg -loop 1 -t 3 -framerate 60 -i image2.jpg -loop 1 -t 3 -framerate 60 -i image3.jpg -filter_complex "[0]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1[s0]; [1]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1[s1]; [2]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1[s2]; [s0][s1]xfade=transition=circleopen:duration=1:offset=2[f0]; [f0][s2]xfade=transition=circleopen:duration=1:offset=4" -c:v libx264 -pix_fmt yuv420p output.mp4
